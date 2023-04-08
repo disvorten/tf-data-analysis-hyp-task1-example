612fd31913ca021c559f757dcabe9e0c8d9e054f
disvorten <124072944+disvorten@users.noreply.github.com>
2023-04-08 22:39:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
+
+chat_id = 1109095907
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.06
+    a, b = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='smaller')
+    if b <= alpha:
+        return True
+    else:
+        return False
